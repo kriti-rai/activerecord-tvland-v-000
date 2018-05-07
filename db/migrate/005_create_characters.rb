1b94af926628e60c9788b716f5d907dbb2ea96cb
Kriti Rai <kriti.rai@gmail.com>
2018-05-07 03:57:19 +0000

    automatically backed up by learn

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -2,7 +2,7 @@ class CreateCharacters < ActiveRecord::Migration[4.2]
   def change
     create_table :characters do |t|
       t.string :name
-      foreign_key :show_id
+      add_foreign_key :show_id
     end
   end
 end
