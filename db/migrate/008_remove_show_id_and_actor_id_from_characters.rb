88a960e375742f91b2b4c42c4b4b632aa82052bc
Kriti Rai <kriti.rai@gmail.com>
2018-05-07 04:32:03 +0000

    automatically backed up by learn

--- db/migrate/008_remove_show_id_and_actor_id_from_characters.rb
+++ db/migrate/008_remove_show_id_and_actor_id_from_characters.rb
@@ -1,5 +1,6 @@
 class RemoveShowIdAndActorIdFromCharacters < ActiveRecord::Migration[4.2]
   def change
-    remove_column :show_id, :character_id
+    remove_column :characters, :show_id
+    remove_column :characters, :actor_id
   end
 end
