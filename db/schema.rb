0a174e42e2b9ff211cfd8e820a396eaf3b92c88b
Kriti Rai <kriti.rai@gmail.com>
2018-05-07 04:21:16 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 5) do
+ActiveRecord::Schema.define(version: 6) do
 
   create_table "actors", force: :cascade do |t|
     t.string "first_name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 5) do
   end
 
   create_table "characters", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "genres", force: :cascade do |t|
     t.string "name"
   end
 
