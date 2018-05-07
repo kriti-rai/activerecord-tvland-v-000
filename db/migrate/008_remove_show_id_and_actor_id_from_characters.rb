43f6ff6e9735c550f8452596b7410503c6340cf5
Kriti Rai <kriti.rai@gmail.com>
2018-05-07 04:30:53 +0000

    automatically backed up by learn

--- db/migrate/008_remove_show_id_and_actor_id_from_characters.rb
+++ db/migrate/008_remove_show_id_and_actor_id_from_characters.rb
@@ -1,7 +1,4 @@
-class RemoveShowIdAndActorId < ActiveRecord::Migration[4.2]
-  #to make an additive change to a schema we create
-  #a new migration, and then in the change method,
-  #we make the change.
+class RemoveShowIdAndActorIdFromCharacters < ActiveRecord::Migration[4.2]
   def change
     remove_column :show_id, :character_id
   end
